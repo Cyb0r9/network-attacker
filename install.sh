a809a2876e46ef52a003e67f2449443c55a58424
Tunisian Eagles <43474676+TunisianEagles@users.noreply.github.com>
2018-09-21 16:29:49 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#Check
+#bash
+if which bash >/dev/null; then
+		sleep 0.25
+    echo "[BASH] Installed [✓]"
+else
+		sleep 0.25
+	echo "[BASH] Not Installed [✗]"
+			sleep 1
+		echo "Install Bash BY Typing [sudo apt-get install bash]"
+fi
+#airmon-ng
+if which airmon-ng >/dev/null; then
+		sleep 0.25
+    echo "[AIRMONG-NG] Installed [✓]"
+else
+		sleep 0.25
+	echo "[AIRMONG-NG] Not Installed [✗]"		
+			sleep 1
+		echo "Install Bash BY Typing [sudo apt-get install airckrack-ng]"
+		sleep 0.25
+fi
+#airodump-ng
+if which airodump-ng >/dev/null; then
+		sleep 0.25
+    echo "[AIRODUMP-NG] Installed [✓]"
+else
+		sleep 0.25
+	echo "[AIRODUMP-NG] Not Installed [✗]"
+			sleep 1
+		echo "Install Bash BY Typing [sudo apt-get install airckrack-ng]"
+fi
+#mdk3
+if which mdk3 >/dev/null; then
+		sleep 0.25
+    echo "[MDK3] Installed [✓]"
+else
+		sleep 0.25
+	echo "[MDK3] Not Installed [✗]"
+			sleep 1
+		echo "Clone And Install Mdk3 From [https://github.com/wi-fi-analyzer/mdk3-master]."
+fi
+#git
+if which git >/dev/null; then
+                sleep 0.25
+    echo -e "$green[GIT]$nc Installed [$green✓$nc]"
+                sleep 0.25
+else
+                sleep 0.25
+        echo -e "$green[GIT]$nc Not Installed [$red✗$nc]"
+                        sleep 1
+                echo "Install [ GIT ] By typing : sudo apt-get install git"
+                exit 1
+fi
+
+#EOF
