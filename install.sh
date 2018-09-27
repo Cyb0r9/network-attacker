52cbbc1c1715a2bcbbd41dfd9f58e6ad25b4fca0
Tunisian Eagles <43474676+TunisianEagles@users.noreply.github.com>
2018-09-27 22:06:52 +0100

    install requirements

--- install.sh
+++ install.sh
@@ -2,44 +2,52 @@
 #Check
 #bash
 if which bash >/dev/null; then
-		sleep 0.25
+                sleep 0.25
     echo "[BASH] Installed [✓]"
 else
-		sleep 0.25
-	echo "[BASH] Not Installed [✗]"
-			sleep 1
-		echo "Install Bash BY Typing [sudo apt-get install bash]"
+                sleep 0.25
+        echo "[BASH] Not Installed [✗]"
+                        sleep 1
+                echo "Install Bash "
+                        sudo apt-get install bash
 fi
 #airmon-ng
 if which airmon-ng >/dev/null; then
-		sleep 0.25
+                sleep 0.25
     echo "[AIRMONG-NG] Installed [✓]"
 else
-		sleep 0.25
-	echo "[AIRMONG-NG] Not Installed [✗]"		
-			sleep 1
-		echo "Install Bash BY Typing [sudo apt-get install airckrack-ng]"
-		sleep 0.25
+                sleep 0.25
+        echo "[AIRMONG-NG] Not Installed [✗]"           
+                        sleep 1
+                echo "Install airmong-ng"
+                        sudo apt-get install airckrack-ng
+                sleep 0.25
 fi
 #airodump-ng
 if which airodump-ng >/dev/null; then
-		sleep 0.25
+                sleep 0.25
     echo "[AIRODUMP-NG] Installed [✓]"
 else
-		sleep 0.25
-	echo "[AIRODUMP-NG] Not Installed [✗]"
-			sleep 1
-		echo "Install Bash BY Typing [sudo apt-get install airckrack-ng]"
+                sleep 0.25
+        echo "[AIRODUMP-NG] Not Installed [✗]"
+                        sleep 1
+                echo "Install airodump-ng "
+                        sudo apt-get install airckrack-ng
 fi
 #mdk3
 if which mdk3 >/dev/null; then
-		sleep 0.25
+                sleep 0.25
     echo "[MDK3] Installed [✓]"
 else
-		sleep 0.25
-	echo "[MDK3] Not Installed [✗]"
-			sleep 1
-		echo "Clone And Install Mdk3 From [https://github.com/wi-fi-analyzer/mdk3-master]."
+                sleep 0.25
+        echo "[MDK3] Not Installed [✗]"
+                        sleep 1
+                echo "Cloning And Installing Mdk3"
+git clone https://github.com/wi-fi-analyzer/mdk3-master
+cd mdk3-master
+make
+sudo make install
+cd ..
 fi
 #git
 if which git >/dev/null; then
@@ -50,8 +58,10 @@ else
                 sleep 0.25
         echo -e "$green[GIT]$nc Not Installed [$red✗$nc]"
                         sleep 1
-                echo "Install [ GIT ] By typing : sudo apt-get install git"
+                echo "Install git "
+                        sudo apt-get install git
                 exit 1
 fi
+
 
 #EOF
